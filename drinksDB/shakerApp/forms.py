57e7de81851038f13f2e4f51dbd22beff39013fc
boryswnr <borysweiner@gmail.com>
2022-05-30 19:31:01 +0200

    added grouped ingredient view, added addDrink view

--- drinksDB/shakerApp/forms.py
+++ drinksDB/shakerApp/forms.py
@@ -1,22 +1,14 @@
 from django import forms
-from .fields import GroupedModelChoiceField
-from .models import requiredUtensil, ingredientType, Ingredients, Type
+# from .fields import GroupedModelChoiceField
+from .models import requiredUtensil, ingredientType, Ingredients
 
 
 class DrinkForm(forms.Form):
     name = forms.CharField(max_length=100)
     utensil = forms.ChoiceField(choices=requiredUtensil)
     ingredients = forms.ModelChoiceField(queryset=Ingredients.objects.all(), widget=forms.Select())
-    # ingredient1 = forms.ModelChoiceField(queryset=Ingredients.objects.all())
 
 
 class IngredientForm(forms.Form):
     name = forms.CharField(max_length=100, label="Ingredient name")
-    type = GroupedModelChoiceField(
-        queryset=Type.objects.exclude(parent=None),
-        choices_groupby='parent'
-    )
-
-    class Meta:
-        model = Ingredients
-        fields = 'name'
+    type = forms.ChoiceField(choices=ingredientType)
--- drinksDB/shakerApp/models.py
+++ drinksDB/shakerApp/models.py
@@ -12,20 +12,9 @@ requiredUtensil = (
     (2, "Shaker")
 )
 
-"""Defining a type as a class will allow to make a group ChoiceField
-    despite using ModelChoiceField and not regular ChoiceField at DrinkRecipe model"""
-
-
-class Type(models.Model):
-    name = models.IntegerField(choices=ingredientType)
-    parent = models.ForeignKey('Type', on_delete=models.CASCADE, null=True)
-
-    def __str__(self):
-        return self.name
-
 
 class Ingredients(models.Model):
-    type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    type = models.IntegerField(choices=ingredientType)
     name = models.CharField(max_length=100)
 
     def __str__(self):
